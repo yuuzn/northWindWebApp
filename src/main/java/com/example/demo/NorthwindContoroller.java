9e1ff15ff2a422bf480b887fffbd11b375e11c91
蘇 東川 <dongsu.workout@outlook.jp>
2019-07-04 21:01:14 +0900

    fixed delete 404 error

--- src/main/java/com/example/demo/NorthwindContoroller.java
+++ src/main/java/com/example/demo/NorthwindContoroller.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -47,8 +48,9 @@ public class NorthwindContoroller {
 		return "memo/edit";
 	}
 
-	@GetMapping("delete")
-	public String delete(Model model) {
+	@GetMapping("delete/{id}")
+	public String delete(@PathVariable Integer id, Model model)
+	{
 		return "memo/delete";
 	}
 }
